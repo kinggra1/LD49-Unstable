89f4057e49c357863c448249bad64651d2de0f15
Danielle Schugars <danielle.schugars@gmail.com>
2021-10-03 02:05:15 -0400

    add animation to slime

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : Singleton<GameManager> {
     public Conversation introConversation;
 
     void Start() {
-        DialogueSystem.Instance.StartConversation(introConversation);
+        //DialogueSystem.Instance.StartConversation(introConversation);
     }
 
     // Update is called once per frame
