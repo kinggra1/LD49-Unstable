651c5e735ae0ac0eea5e1ca9a301e7341be912f3
Danielle Schugars <danielle.schugars@gmail.com>
2021-10-03 01:33:49 -0400

    let bat shoot projectiles

--- Assets/Scripts/Enemies/BatEnemy.cs
+++ Assets/Scripts/Enemies/BatEnemy.cs
@@ -6,8 +6,12 @@ public class BatEnemy : MonoBehaviour, EnemyInterface {
     private static readonly float SPEED = 1f;
     private static readonly int MAX_HEALTH = 1;
     private static readonly float INSTABILITY = 0.1f;
+    private static readonly float SHOOT_TIME = 2f;
 
     private static int currentHealth = MAX_HEALTH;
+    private static float shootCooldown = 0f;
+
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start() {
@@ -20,6 +24,12 @@ public class BatEnemy : MonoBehaviour, EnemyInterface {
         float step = SPEED * Time.deltaTime; // calculate distance to move
         var playerPosition = CharacterController.Instance.transform.position;
         this.transform.position = Vector3.MoveTowards(this.transform.position, playerPosition, step);
+
+        shootCooldown -= Time.deltaTime;
+        if (shootCooldown <= 0) {
+            shootCooldown = SHOOT_TIME;
+            Shoot();
+        }
     }
 
     public void TakeDamage(int damageAmount) {
@@ -39,5 +49,14 @@ public class BatEnemy : MonoBehaviour, EnemyInterface {
         if (player != null) {
             player.TakeDamage(INSTABILITY);
         }
+    }
+    private void Shoot() {
+        var playerPosition = CharacterController.Instance.transform.position;
+        Vector3 projectileDirection = (playerPosition - this.transform.position).normalized;
+        
+        GameObject projectile = Instantiate(projectilePrefab);
+        projectile.transform.position = this.transform.position + projectileDirection.normalized;
+        EnemyProjectileInterface projectileScript = projectile.GetComponent<EnemyProjectileInterface>();
+        projectileScript.SetDirection(projectileDirection);
     }
 }
---
+++ Assets/Scripts/Enemies/EnemyProjectiles/BatProjectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatProjectile : MonoBehaviour, EnemyProjectileInterface {
+    private static readonly float SPEED = 5f;
+    private static readonly float INSTABILITY = 0.1f;
+
+    private Vector3 direction;
+
+    public void SetDirection(Vector3 direction) {
+        this.direction = direction.normalized;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.position += direction * SPEED * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        // if object collided with is player, inflict damage
+        var player = collider.gameObject.GetComponent<CharacterController>();
+        if (player != null) {
+            player.TakeDamage(INSTABILITY);
+
+        }
+        Destroy(this.gameObject);
+    }
+}
